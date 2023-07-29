526957ce932ef3552bbbdf37645bd6483969c07d
Jikanio <50619947+Jikanio@users.noreply.github.com>
2023-07-29 14:03:51 +0300

    Add files via upload

--- Readdocs.py
+++ Readdocs.py
@@ -1,49 +1,57 @@
 import streamlit as st
-import pandas as pd
-import requests
-import openai
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
-# Set your OpenAI API key here
-openai.api_key = 'YOUR_OPENAI_API_KEY'
+# Function to generate a response using GPT-3.5 model
+def generate_response(prompt_text, model, tokenizer):
+    inputs = tokenizer(prompt_text, return_tensors="pt")
+    outputs = model(**inputs)
+    response = tokenizer.decode(outputs.logits[0], skip_special_tokens=True)
+    return response
 
-def read_excel(file):
-    df = pd.read_excel(file)
-    return df.to_string(index=False)
-
-def read_web_link(url):
-    response = requests.get(url)
-    return response.text
-
+# Main Streamlit app code
 def main():
-    st.title("Upload and Read Documents")
-    st.write("Please upload an Excel file or provide a web link.")
+    st.title("GPT-3.5 Chatbox")
 
-    file = st.file_uploader("Upload an Excel file", type=["xlsx"])
+    # Uploading documents
+    uploaded_files = st.file_uploader("Upload Documents", accept_multiple_files=True)
 
-    if file:
-        text = read_excel(file)
-        st.subheader("Uploaded Document Text:")
-        st.write(text)
+    # Input web link
+    web_link = st.text_input("Web Link")
 
-    web_link = st.text_input("Provide a web link:")
+    # Input user prompt
+    user_prompt = st.text_area("Enter your prompt here:")
+
+    # Load GPT-3.5 model and tokenizer
+    model = GPT2LMHeadModel.from_pretrained("gpt2")
+    tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+
+    # Process uploaded documents
+    if uploaded_files:
+        for file in uploaded_files:
+            file_content = file.read()
+            st.write(f"**Uploaded Document:** {file.name}")
+            st.write(file_content)
+
+            # Generate response based on the uploaded document
+            response = generate_response(file_content.decode("utf-8"), model, tokenizer)
+            st.write(f"**GPT-3.5 Response:** {response}")
+
+    # Process web link
     if web_link:
-        text = read_web_link(web_link)
-        st.subheader("Web Link Text:")
-        st.write(text)
+        st.write(f"**Web Link:** {web_link}")
 
-    if file or web_link:
-        user_input = st.text_input("Ask a question or provide a prompt:")
-        if user_input:
-            response = openai.Completion.create(
-                model="text-davinci-003",
-                prompt=text + "\nUser: " + user_input + "\nAI:",
-                temperature=0.7,
-                max_tokens=150,
-                stop=["\n"],
-            )
-            ai_response = response.choices[0].text.strip()
-            st.subheader("AI Response:")
-            st.write(ai_response)
+        # Generate response based on the web link
+        # You might need to use a library like 'requests' to fetch the data from the web link
+        response = generate_response(web_link, model, tokenizer)
+        st.write(f"**GPT-3.5 Response:** {response}")
+
+    # Process user prompt
+    if user_prompt:
+        st.write(f"**Your Prompt:** {user_prompt}")
+
+        # Generate response based on the user prompt
+        response = generate_response(user_prompt, model, tokenizer)
+        st.write(f"**GPT-3.5 Response:** {response}")
 
 
 if __name__ == "__main__":
