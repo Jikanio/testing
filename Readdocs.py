8f49d858ffefb37381fd12e8bf77ca0c4a9898bd
Jikanio <50619947+Jikanio@users.noreply.github.com>
2023-07-29 13:32:26 +0300

    Add files via upload

--- Readdocs.py
+++ Readdocs.py
@@ -1,14 +1,18 @@
 import streamlit as st
 import pandas as pd
 import requests
-import docx2txt
+import PyPDF2
 import openai
 
 # Set your OpenAI API key here
-openai.api_key = 'mxvJfnZqi2jGGtOtab66T3BlbkFJ4HTPilTcYW8ZtKEnLIyW'
+openai.api_key = 'YOUR_OPENAI_API_KEY'
 
-def read_docx(file):
-    text = docx2txt.process(file)
+def read_pdf(file):
+    text = ""
+    pdf_reader = PyPDF2.PdfFileReader(file)
+    for page_num in range(pdf_reader.numPages):
+        page = pdf_reader.getPage(page_num)
+        text += page.extractText()
     return text
 
 def read_excel(file):
@@ -21,17 +25,14 @@ def read_web_link(url):
 
 def main():
     st.title("Upload and Read Documents")
-    st.write("Please upload a Word, PDF, or Excel file, or provide a web link.")
+    st.write("Please upload a PDF, Excel file, or provide a web link.")
 
-    file = st.file_uploader("Upload a file", type=["docx", "pdf", "xlsx"])
+    file = st.file_uploader("Upload a file", type=["pdf", "xlsx"])
 
     if file:
         file_type = file.type
-        if file_type == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
-            text = read_docx(file)
-        elif file_type == 'application/pdf':
-            # Add code to read PDF here if needed
-            text = "PDF file format is not supported yet."
+        if file_type == 'application/pdf':
+            text = read_pdf(file)
         elif file_type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
             text = read_excel(file)
         else:
